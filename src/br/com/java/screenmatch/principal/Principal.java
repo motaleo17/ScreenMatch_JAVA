ca24a2adcd95eb08b95a49f207ab5a2d227eccb7
Leonardo Mota <leonardo.mota.17@hotmail.com>
2024-10-30 11:05:02 -0300

    teste commit

--- src/br/com/java/screenmatch/principal/Principal.java
+++ src/br/com/java/screenmatch/principal/Principal.java
@@ -1,35 +1,33 @@
+package br.com.java.screenmatch.principal;
+
 import br.com.java.screenmatch.calculo.CalculadorDeTempo;
 import br.com.java.screenmatch.calculo.FiltroRecomendacao;
 import br.com.java.screenmatch.modelos.Episodio;
 import br.com.java.screenmatch.modelos.Filme;
 import br.com.java.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme = new Filme("O poderoso chefão",1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duracao do Filme: " + meuFilme.getDuracaoEmMinutos());
-
+        //Ricci Gente boa e gay
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
         System.out.println("Total de avaliacoes: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(200);
+        Serie lost = new Serie("Lost",2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duracao em minutos para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
+        Filme outroFilme = new Filme("Avatar",2023);
         outroFilme.setDuracaoEmMinutos(200);
 
         CalculadorDeTempo calculadora = new CalculadorDeTempo();
@@ -46,5 +44,18 @@ public class Principal {
         ep.setSerie(lost);
         ep.setTotalVisualizacao(300);
         filtro.filtra(ep);
+
+        var maisFilme = new Filme("Velozes e Furiosos",2007);
+        maisFilme.setDuracaoEmMinutos(180);
+        maisFilme.avalia(10);
+
+        var listaFilmes = new ArrayList<Filme>();
+        listaFilmes.add(maisFilme);
+        listaFilmes.add(outroFilme);
+        listaFilmes.add(meuFilme);
+        System.out.println("A lista possui: " + listaFilmes.size() + " filmes");
+        System.out.println("Primeiro Filme: " + listaFilmes.get(0).getNome());
+        System.out.println(listaFilmes);
+        System.out.println("Primeiro Filme: " + listaFilmes.get(0).toString());
     }
 }
---
+++ src/br/com/java/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,37 @@
+package br.com.java.screenmatch.principal;
+
+import br.com.java.screenmatch.modelos.Filme;
+import br.com.java.screenmatch.modelos.Serie;
+import br.com.java.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão",1970);
+        meuFilme.avalia(7);
+
+        Filme outroFilme = new Filme("Avatar",2023);
+        outroFilme.avalia(8);
+
+        var maisFilme = new Filme("Velozes e Furiosos",2007);
+        maisFilme.avalia(10);
+
+        Serie lost = new Serie("Lost",2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(maisFilme);
+        lista.add(outroFilme);
+        lista.add(meuFilme);
+        lista.add(lost);
+
+        for (Titulo item : lista){
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2){
+                System.out.println("Classificacao: " + filme.getClassificacao());
+            }
+
+        }
+
+    }
+}
